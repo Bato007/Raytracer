ee19d0da303b0c0b974fa3a76c02edd76b8aa36f
Brandon <brandonhernandez_10@outlook.es>
2021-12-03 11:57:02 -0600

    Fixed

--- trace.py
+++ trace.py
@@ -118,17 +118,53 @@ glass = Material(color(150, 180, 200), albedo=(0, 0.5, 0.1, 0.8), spec=125, refr
 iron = Material(color(129,126,121), albedo=[0.95, 0.01, 0.3, 0], spec=100)
 grass = Material(color(52,140,49), albedo=[0.6, 0.2, 0, 0], spec=12)
 wood = Material(color(202, 164, 114), albedo=[0.5, 0.5, 0, 0], spec=10)
-
+zombieSkin = Material(color(70, 110, 51), albedo=[0.5, 0.5, 0, 0], spec=10)
+zombiePants = Material(color(69, 54, 155), albedo=[0.9, 0.1, 0, 0], spec=10)
+zombieTshirt = Material(color(35, 180, 166), albedo=[0.9, 0.1, 0, 0], spec=10)
+zombieShoes = Material(color(100, 100, 102), albedo=[0.9, 0.1, 0, 0], spec=10)
+eyes = Material(color(10, 10, 10), albedo=[0.5, 0.5, 0, 0], spec=10)
+water = Material(color(150, 180, 200), albedo=(0, 0.5, 0.1, 0.7), spec=100, refractive_index=1.75)
 model = Obj('./tv.obj')
 triangles = model.loadTriangles(wood)
 
+def zombie(x, y, z=-10):
+    return [
+        Cube(V3(x, y, z), V3(2, 2, 2), zombieSkin),
+        Cube(V3(x+0.25, y, z+1), V3(1/4, 1/4, 1/4), eyes),
+        Cube(V3(x-0.25, y, z+1), V3(1/4, 1/4, 1/4), eyes),
+        Cube(V3(x-1, y+1.5, z), V3(1, 1, 3), zombieSkin),
+        Cube(V3(x+1, y+1.5, z), V3(1, 1, 3), zombieSkin),
+        Cube(V3(x, y+2, z), V3(1.75, 3, 2), zombieTshirt),
+        Cube(V3(x+0.5, y+4, z), V3(3/4, 2, 1.5), zombiePants),
+        Cube(V3(x-0.5, y+4, z), V3(3/4, 2, 1.5), zombiePants),
+    ]
+
 r.scene = [
+    # Cube(V3(0, 5, 10), V3(20, 10, 20), water),
     Cube(V3(8, 3, -15), V3(2, 3, 7), ivory),
     Cube(V3(8, 1, -8), V3(2, 2, 2), glass),
-    Cube(V3(8, 2, -20), V3(8, 5, 1), mirror),
-    Cube(V3(8, -4, -17), V3(5, 5, 5), iron),
-    Triangle((V3(0, -6, -20), V3(5, -7, -20), V3(-4, -10, -20)), grass)
+    Cube(V3(8, 2, -25), V3(8, 5, 1), mirror),
+    Cube(V3(8, -11, -20), V3(2, 2, 2), iron),
+    Cube(V3(7.25, -11.25, -19), V3(1/4, 1/4, 1/4), eyes),
+    Cube(V3(8.5, -11.25, -19), V3(1/4, 1/4, 1/4), eyes),
+    Cube(V3(8, -11, -18.5), V3(1/2, 1, 1/2), iron),
+    Cube(V3(8, -8, -20), V3(6, 4, 2), iron),
+    Cube(V3(8, -5, -20), V3(3, 2, 2), iron),    
+    Cube(V3(7, -3, -20), V3(1, 4, 1), iron),
+    Cube(V3(9, -3, -20), V3(1, 4, 1), iron),
+    Cube(V3(11.5, -7, -20), V3(1, 6, 1), iron),
+    Cube(V3(4.5, -7, -20), V3(1, 6, 1), iron),
+    Triangle((V3(0, -6, -20), V3(5, -7, -25), V3(-4, -10, -20)), grass),
 ]
+
+zombies = [
+    zombie(-6, 0),
+    zombie(0, 5),
+    zombie(6, 4),
+]
+for zombie in zombies:
+    for cube in zombie:
+        r.scene.append(cube)
 
 for triangle in triangles:
     r.scene.append(triangle)
